a29ea1a8b2c7b50914d65d854c41531e64038f02
qayserily <muratbayram2005@gmail.com>
2021-08-04 18:50:49 +0300

    cucumber.json created

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports"},
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@login"
+        dryRun = true,
+        tags = "@wip"
 )
 public class CukesRunner {
 }
