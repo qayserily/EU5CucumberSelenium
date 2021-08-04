f69e80661bc798b553b8b12e570f27d46b489f26
qayserily <muratbayram2005@gmail.com>
2021-08-04 16:33:47 +0300

    cucumber.json created

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,26 +1,44 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks {
+
     @Before
     public void setUp(){
-        System.out.println("This is coming from BEFORE");
+        System.out.println("\tthis is coming from BEFORE");
+        Driver.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
-    public void tearDown(){
-        System.out.println("This is coming from AFTER");
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
     }
 
-    @Before("@db")
+    @Before("@abc")
     public void setUpdb(){
-        System.out.println("Connecting to database...");
+        System.out.println("\tconnecting to database...");
     }
 
-    @After("@db")
+    @After("@abc")
     public void closeDb(){
-        System.out.println("Disconnecting to database...");
+        System.out.println("\tdisconnecting to database...");
+
     }
+
+
+
 }
